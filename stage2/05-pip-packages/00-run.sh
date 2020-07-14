25ad4904dd8fa63b0caa90e93fc76d4d9812427b
Doyeon <archdo0909@gmail.com>
2020-07-14 10:10:45 +0900

    Update pip installer

--- stage2/05-pip-packages/00-run.sh
+++ stage2/05-pip-packages/00-run.sh
@@ -15,6 +15,14 @@ pushd ${DOWNLOAD_DIR}
 wget -nc -nv -O pyserial.tar.gz \
     https://github.com/pyserial/pyserial/releases/download/v3.2.1/pyserial-3.2.1.tar.gz
 
+# wrapt
+wget -nc -nv -O wrapt.tar.gz \
+    https://github.com/GrahamDumpleton/wrapt/archive/1.12.1.tar.gz
+
+# aenum
+wget -nc -nv -O aenum.tar.gz \
+    https://bitbucket.org/stoneleaf/aenum/get/2.1.3.tar.gz
+
 # python can
 wget -nc -nv -O python-can.tar.gz \
     https://github.com/hardbyte/python-can/archive/3.3.3.tar.gz
@@ -22,10 +30,6 @@ wget -nc -nv -O python-can.tar.gz \
 # pymodi git clone
 wget -nc -nv -O pymodi.tar.gz \
     https://github.com/LUXROBO/pymodi/archive/v0.9.0.tar.gz
-
-# Jupyter notebook
-wget -nc -nv -O notebook.tar.gz \
-    https://github.com/jupyter/notebook/archive/6.1.0rc1.tar.gz
 
 popd
 
@@ -41,6 +45,14 @@ tar xzf "${DOWNLOAD_DIR}/pyserial.tar.gz"
 mv pyserial-* pyserial
 echo "Pyserial src downloaded"
 
+tar xzf "${DOWNLOAD_DIR}/wrapt.tar.gz"
+mv wrapt-* wrapt
+echo "Wrapt src downloaded"
+
+tar xzf "{DOWNLOAD_DIR}/aenum.tar.gz"
+mv stoneleaf-* aenum
+echo "aenum src downloaded"
+
 tar xzf "${DOWNLOAD_DIR}/python-can.tar.gz"
 mv python-can-* python-can
 echo "python can src downloaded"
@@ -49,9 +61,6 @@ tar xzf "${DOWNLOAD_DIR}/pymodi.tar.gz"
 mv pymodi-* pymodi
 echo "pymodi src downloaded"
 
-tar xzf "${DOWNLOAD_DIR}/notebook.tar.gz"
-mv notebook-* notebook
-echo "jupyter notebook src downloaded"
 
 # pyserial
 on_chroot << EOF
@@ -63,18 +72,32 @@ EOF
 
 echo "pyserial installation done!"
 
-# python-can
+# wrapt 
 on_chroot << EOF
-pushd /usr/src/python-can
-python3 -m pip install aenum-2.2.3-py3-none-any.whl -f ./ --no-index
-popd
-pushd /usr/src/python-can/python-can-3.3.3
-python3 setup.py install
-popd
-pushd /usr/src/python-can/wrapt-1.12.1
+pushd /usr/src/wrapt
 python3 setup.py install
 popd
 EOF
+
+echo "wrapt installation done!"
+
+# aenum
+on_chroot << EOF
+pushd /usr/src/aenum
+python3 setup.py install
+popd
+EOF
+
+echo "aenum installation done!"
+
+# python-can
+on_chroot << EOF
+pushd /usr/src/python-can
+python3 setup.py install
+popd
+EOF
+
+echo "python-can installation done!"
 
 # pymodi
 on_chroot << EOF
@@ -83,11 +106,3 @@ python3 setup.py install
 popd 
 EOF
 echo "pymodi installed"
-
-# jupyter notebook
-on_chroot << EOF
-pushd /usr/src/notebook
-python3 setup.py install
-popd
-EOF
-echo "jupyter notebook installed"
